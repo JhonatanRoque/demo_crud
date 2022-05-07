f4ca849b8a7d26fb3848aee641127410f9680252
Neftaly-Android-PC <neftalycalderon16@gmail.com>
2022-05-06 19:24:20 -0600

    Hola Mundo

--- src/demo_crud/Neftaly_C.java
+++ src/demo_crud/Neftaly_C.java
@@ -4,5 +4,6 @@ package demo_crud;
 public class Neftaly_C {
     public static void main(String[] args) {
         System.out.println("Neftaly Calderon");
+        System.out.println("Hola Mundo");
     }
 }
