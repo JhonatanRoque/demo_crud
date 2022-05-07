de1ea83f209d61b512099a9ddc9123bf5f0e0e62
Criss7w7 <101343214+Criss7w7@users.noreply.github.com>
2022-05-06 18:54:07 -0600

    Clase Cristina

--- src/demo_crud/cristina.java
+++ src/demo_crud/cristina.java
@@ -7,16 +7,19 @@ public static void main(String[] args){
     int i,j=0,bra='t';
     for(i=0;i<=797;i=i)
     {
-       
         if(bra=='t')
         {
             i=i+2;
             bra='f';
         }
-else
+ else
         {
             i=i+3;
             bra='t';
         }
+        System.out.println(+i);
+    }
+}
 
+}
 
